6d6c948ee3b11f14f9b84a3071f9df97b9200d49
Will Snavely <will.snavely@gmail.com>
2026-02-09 08:06:00 -0500

    Working on products page

--- app/View/Composers/Shop.php
+++ app/View/Composers/Shop.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 namespace App\View\Composers;
 
 use Roots\Acorn\View\Composer;
@@ -11,42 +11,46 @@ class Shop extends Composer
         'woocommerce.taxonomy-product_cat',
     ];
 
-    private function orderTerms($terms, $term_order) {
+    private function orderTerms($terms, $term_order)
+    {
         $by_slug = [];
-        foreach($terms as $term) {
+        foreach ($terms as $term) {
             $by_slug[$term->slug] = $term;
         }
         $result = [];
-        foreach($term_order as $term) {
-            if(isset($by_slug[$term])) {
+        foreach ($term_order as $term) {
+            if (isset($by_slug[$term])) {
                 $result[] = $by_slug[$term];
             }
         }
         return $result;
     }
 
-    public function getEventTerms() {
+    public function getEventTerms()
+    {
         $terms = get_terms(['taxonomy' => 'event_type', 'hide_empty' => true]);
         return $this->orderTerms($terms, ['clinic', 'cardio-tennis', 'tournament']);
     }
 
-    public function getLevelTerms() {
+    public function getLevelTerms()
+    {
         $terms = get_terms(['taxonomy' => 'skill_level', 'hide_empty' => true]);
         return $this->orderTerms($terms, ['beginner', 'intermediate', 'advanced']);
     }
 
-    public function getAgeGroupTerms() {
+    public function getAgeGroupTerms()
+    {
         $terms = get_terms(['taxonomy' => 'age_group', 'hide_empty' => true]);
-        return $this->orderTerms($terms, ['junior', 'adult']); 
+        return $this->orderTerms($terms, ['junior', 'adult']);
     }
 
     public function with()
     {
         return [
             'event_types' => $this->getEventTerms(),
-            'age_groups'  => $this->getAgeGroupTerms(), 
-            'skill_levels'=> $this->getLevelTerms(), 
-            'active_filters' => $_GET, 
+            'age_groups' => $this->getAgeGroupTerms(),
+            'skill_levels' => $this->getLevelTerms(),
+            'active_filters' => $_GET,
             'filter_url' => function ($taxonomy, $term_slug) {
                 $params = $_GET;
                 if (isset($params[$taxonomy]) && $params[$taxonomy] === $term_slug) {
@@ -54,7 +58,8 @@ class Shop extends Composer
                 } else {
                     $params[$taxonomy] = $term_slug;
                 }
-                return add_query_arg(array_filter($params), get_permalink(wc_get_page_id('shop')));
+                $base_url = get_post_type_archive_link('product');
+                return add_query_arg(array_filter($params), $base_url);
             },
         ];
     }
